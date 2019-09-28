525e2bf88503346003257e21ba0189b01c58054f
buckeyeburl <tkgwheeler@gmail.com>
2019-09-28 12:11:13 +0100

    Added Weighted Treasure finder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Table Top RPG Generators`,
+    description: `General dick around whilst trying to find a fun project to work on`,
+    author: `Tom Wheeler`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/Treasure/Treasure.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Treasure = () => {
+  const [treasure, setTreasure] = useState("");
+
+  const weightedRandomBag = items => {
+    let entries = [];
+    let accumulatedWeight = 0;
+    let random;
+    let foundItem;
+
+    items.forEach(x => {
+      accumulatedWeight += x.weight;
+      entries.push({ name: x.name, weight: accumulatedWeight });
+    });
+
+    random = Math.random() * accumulatedWeight;
+    foundItem = entries.find(entry => entry.weight >= random);
+    setTreasure(foundItem.name);
+  };
+
+  return (
+    <div>
+      <h1>Treasure</h1>
+      <h2>{treasure}</h2>
+      <button onClick={() => weightedRandomBag(treasureTypes)}>
+        Treasure Type
+      </button>
+    </div>
+  );
+};
+
+export default Treasure;
+
+const treasureTypes = [
+  {
+    name: "Art",
+    weight: 1,
+  },
+  {
+    name: "Jeweled Items",
+    weight: 2,
+  },
+  {
+    name: "Goods",
+    weight: 4,
+  },
+  {
+    name: "Coins",
+    weight: 6,
+  },
+];
